660630bd70b5b26dc345aeae83ee459c1f18b0dd
Xiulan Shi <xiulanshi2013@gmail.com>
2015-10-31 10:49:28 -0400

    Created Xcode Project

---
+++ WomenOfSiliconAlley/WomenOfSiliconAlley/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WomenOfSiliconAlley
+//
+//  Created by Xiulan Shi on 10/31/15.
+//  Copyright © 2015 Xiulan Shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WomenOfSiliconAlley/WomenOfSiliconAlley/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WomenOfSiliconAlley
+//
+//  Created by Xiulan Shi on 10/31/15.
+//  Copyright © 2015 Xiulan Shi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
