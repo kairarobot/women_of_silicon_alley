c2dfce6b5d6646872a25131a0ebd3dd1611843cc
Xiulan Shi <xiulanshi2013@gmail.com>
2015-10-31 12:19:38 -0400

    Updated TableView

--- WomenOfSiliconAlley/WomenOfSiliconAlley/ViewController.m
+++ WomenOfSiliconAlley/WomenOfSiliconAlley/ViewController.m
@@ -9,6 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+<
+UITableViewDataSource,
+UITableViewDelegate
+>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@property (nonatomic) NSMutableArray *storyArray;
 
 @end
 
@@ -17,10 +24,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
-    testObject[@"foo"] = @"bar";
-    [testObject saveInBackground];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    
+//    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
+//    testObject[@"foo"] = @"bar";
+//    [testObject saveInBackground];
 }
+
+
+#pragma Mark - TableView Delegate Methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.storyArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ImageCell" forIndexPath:indexPath];
+    
+    cell.textLabel.text = self.storyArray[indexPath.row];
+    
+    return cell;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
